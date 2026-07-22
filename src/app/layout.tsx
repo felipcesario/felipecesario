7a2a8809a7e86d8ad396650fb38787ca423c4163
Rafael Vecchi <rafael.vecchi@grafeno.digital>
2026-07-22 22:26:33 +0200

    insert gtm1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export const metadata: Metadata = {
   }
 }
 
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-M4ZCX99R";
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-M4ZCX99";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
