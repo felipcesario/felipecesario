d9a5c197f83c702df0bceefbcd1f7b1ca7cc6754
Rafael Vecchi <rafael.vecchi@grafeno.digital>
2026-07-22 23:12:01 +0200

    insert e-goi 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,7 +84,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <GtmPageView />
         <GtmSectionViews sectionIds={["inicio", "ajuda-urgente", "contato"]} />
 
-        {children}                                    
+        {children}   
+        <script src="https://cdn.egoi.site/1787372_felipecesarioadv.com.br.js" async defer></script>                                                             
       </body>
     </html>
   );
